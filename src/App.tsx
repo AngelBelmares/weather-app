0d3ce38d43a078a144e878a8ae8230dc29e0a6f1
Angel <angelgmb2@hotmail.com>
2024-01-30 01:20:55 -0600

    Add chart.js and react-chartjs-2 dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import { Background } from './components/Background'
-import { WeatherTimeLine } from './components/WeatherTimeLine'
+import { ForecastCards } from './components/ForecastCards'
 import apiRespone from './utils/api_response_example.json'
+import { Line } from 'react-chartjs-2'
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+} from 'chart.js'
+import { sign } from 'chart.js/helpers'
 
 const weatherData = apiRespone.days
 
@@ -18,16 +33,90 @@ const mappedWeatherData = weatherData?.map(day => ({
 }))
 
 function App (): JSX.Element {
+  const [selectedDay, setSelectedDay] = useState(apiRespone.days[0])
+
   return (
     <>
       <Background theme='night' />
-      <main className='flex flex-col items-center justify-between w-full h-96 p-3 md:w-3/4  bg-black'>
-        <WeatherTimeLine
+      <main className='flex flex-col items-center justify-between w-full h-1/2 p-3 md:w-3/4 gap-8'>
+        <ForecastCards
           weatherData={mappedWeatherData}
         />
-        {/* {<WeatherForecast />} */}
+        <TemperatureChart day={selectedDay} />
       </main>
     </>
+  )
+}
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+)
+
+function TemperatureChart ({ day }: any): JSX.Element {
+  const maxTemp = Math.max(...day.hours.map((hour: any) => hour.temp))
+  const minTemp = Math.min(...day.hours.map((hour: any) => hour.temp))
+
+  const labels = ['12 PM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM']
+
+  const options = {
+    scales: {
+      x: {
+        ticks: {
+          color: 'white',
+          font: {
+            size: 14
+          }
+        }
+      },
+      y: {
+        min: minTemp - 3,
+        max: maxTemp + 3,
+        ticks: {
+          color: 'white',
+          font: {
+            size: 14
+          }
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Temperature',
+        color: 'white',
+        font: {
+          size: 20
+        }
+      },
+      filler: {
+        propagate: true
+      }
+    }
+  }
+  const data = {
+    labels,
+    datasets: [{
+      data: day.hours.map(hour => hour.temp),
+      borderColor: 'rgba(255, 255, 255, 0.8)',
+      tension: 0.5,
+      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+      fill: 'start'
+    }]
+  }
+  return (
+    <section className='flex w-full h-full justify-center backdrop-blur-sm'>
+      <Line data={data} options={options} />
+    </section>
   )
 }
 
--- src/components/ForecastCards.tsx
+++ src/components/ForecastCards.tsx
@@ -2,7 +2,7 @@ import { WeatherTimeLineProps } from '../types/weather'
 import { WeatherCard } from './WeatherCard'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
-export function WeatherTimeLine ({ weatherData = [] }: WeatherTimeLineProps): JSX.Element {
+export function ForecastCards ({ weatherData = [] }: WeatherTimeLineProps): JSX.Element {
   if (!Array.isArray(weatherData)) {
     console.log(weatherData)
     return <div>Error: weatherData is not an array</div>
@@ -23,20 +23,22 @@ export function WeatherTimeLine ({ weatherData = [] }: WeatherTimeLineProps): JS
   }
 
   return (
-    <section id='slider' className='realtive flex justify-center items-center max-h-48 w-full gap-2 overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+    <section className='relative flex items-center h-full w-full'>
       <MdChevronLeft className='opacity-70 hover:opacity-100' onClick={slideLeft} size={40} color='white' />
-      {weatherData.map((data, index) => {
-        const { day, imageSrc, temperature, conditions } = data
-        return (
-          <WeatherCard
-            key={index}
-            day={day}
-            imageSrc={imageSrc}
-            temperature={temperature}
-            conditions={conditions}
-          />
-        )
-      })}
+      <div id='slider' className='flex items-center justify-start w-fit gap-2 overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+        {weatherData.map((data, index) => {
+          const { day, imageSrc, temperature, conditions } = data
+          return (
+            <WeatherCard
+              key={index}
+              day={day}
+              imageSrc={imageSrc}
+              temperature={temperature}
+              conditions={conditions}
+            />
+          )
+        })}
+      </div>
       <MdChevronRight className='opacity-70 hover:opacity-100' onClick={slideRight} size={40} color='white' />
     </section>
   )
